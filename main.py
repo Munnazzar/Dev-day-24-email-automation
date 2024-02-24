a70d16825d58e070d817a1d79d3c324e374dcf88
Sarim Ahmed <88588593+thenoisyninga@users.noreply.github.com>
2024-02-24 12:09:33 +0500

    Fixed indexing error while iterating on email records

--- main.py
+++ main.py
@@ -13,48 +13,36 @@ sentMailsCsvPath = "sentRecords.csv"
 unsentRecords = readRecordsFromCsv(unsentMailsCsvPath)
 sentRecords = readRecordsFromCsv(sentMailsCsvPath)
 
-for memberData in unsentRecords:
+print("Unsent Records:")
+for member in unsentRecords:
+    print(member["Email Address"])
+
+print("Sent Records:")
+for member in sentRecords:
+    print(member["Email Address"])
+
+unsentLength = len(unsentRecords)
+i = 0
+
+while i < unsentLength:
+    memberData = unsentRecords[i]
 
     letter = getLetter(
         memberData["FULL NAME"],
         memberData["SELECT ON-DAY TEAM"],
         memberData["SELECT POSITION"],
     )
-    # write to log file incase of unsuccesful email
+    # sort the data according to if the mail was sent or not
     if sendPdfAttachmentMail(memberData["Email Address"], letter) == False:
         pass
     else:
         unsentRecords.remove(memberData)
         sentRecords.append(memberData)
+        i -= 1
+        unsentLength -= 1
+
+    i += 1
 
 
 writeRecordsToCsv(unsentRecords, unsentMailsCsvPath)
 writeRecordsToCsv(sentRecords, sentMailsCsvPath)
-
-# # Creating history folder if doesnt exist
-# if not os.path.exists(os.getcwd() + "/" + historyFolderName):
-#     os.makedirs(os.getcwd() + "/" + historyFolderName, exist_ok=True)
-
-# # Copying the old csv data to history with timestamp
-# shutil.copy2(
-#     unsentMailsCsvPath,
-#     f"{os.curdir}/{historyFolderName}/",
-# )
-
-# now = datetime.datetime.now()
-# os.rename(
-#     f"{os.curdir}/{historyFolderName}/{os.path.basename(unsentMailsCsvPath)}",
-#     f"{os.curdir}/{historyFolderName}/{os.path.basename(unsentMailsCsvPath)[:-4]}-{now.year}-{now.month}-{now.day}-{now.hour}-{now.minute}-{now.second}.csv",
-# )
-
-# # renaming last unsent mails to main csv
-# os.remove(unsentMailsCsvPath)
-
-# # check if unsentMails file exsist then rename it to csv file
-# if os.path.exists(sentMailsCsvPath):
-#     os.rename(sentMailsCsvPath, unsentMailsCsvPath)
-
-# print("=======================================================")
-# print(f"Total records: {lineCount}")
-# print(f"Successful mails: {lineCount- unsuccesfulCount}")
-# print(f"Unsuccessful mails: {unsuccesfulCount}")

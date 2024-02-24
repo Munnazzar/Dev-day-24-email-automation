338fd1d9d4aa7a8ab7c39eb879f1bb614765b859
Sarim Ahmed <88588593+thenoisyninga@users.noreply.github.com>
2024-02-24 16:09:43 +0500

    Added exception handling and formatted error messages.

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from createLetter import getLetter
 from csvWriter import writeRecordsToCsv, readRecordsFromCsv
 from emailHtmlContent import getHtmlContent
 
+print("================================================")
+print("         DEV DAY MEMBER MAILS MANAGER")
+print("================================================\n")
+
 # someway to add group links
-groupLink= "www.google.com"
+groupLink = "www.google.com"
 
 unsentMailsCsvPath = "unsentRecords.csv"
 sentMailsCsvPath = "sentRecords.csv"
@@ -12,43 +16,59 @@ sentMailsCsvPath = "sentRecords.csv"
 unsentRecords = readRecordsFromCsv(unsentMailsCsvPath)
 sentRecords = readRecordsFromCsv(sentMailsCsvPath)
 
-totalRecords= len(unsentRecords)
+totalRecords = len(unsentRecords)
 unsentLength = totalRecords
-i = 0
 
-while i < unsentLength:
-    memberData = unsentRecords[i]
+try:
 
-    letter = getLetter(
-        memberData["FULL NAME"],
-        memberData["SELECT ON-DAY TEAM"],
-        memberData["SELECT POSITION"],
-    )
-    
-    htmlContent= getHtmlContent(groupLink)
+    i = 0
 
-    # sort the data according to if the mail was sent or not
-    if sendPdfAttachmentMail(memberData["Email Address"], letter,htmlContent) == True:
-        unsentRecords.remove(memberData)
-        sentRecords.append(memberData)
-        i -= 1
-        unsentLength -= 1
+    while i < unsentLength:
+        memberData = unsentRecords[i]
 
-    i += 1
+        letter = getLetter(
+            memberData["FULL NAME"],
+            memberData["SELECT ON-DAY TEAM"],
+            memberData["SELECT POSITION"],
+        )
 
+        htmlContent = getHtmlContent(groupLink)
 
-if writeRecordsToCsv(sentRecords, sentMailsCsvPath):
-    print("Sent records written to file")
-else:
-    print("No sent records")
+        # sort the data according to if the mail was sent or not
+        if (
+            sendPdfAttachmentMail(memberData["Email Address"], letter, htmlContent)
+            == True
+        ):
+            unsentRecords.remove(memberData)
+            sentRecords.append(memberData)
+            i -= 1
+            unsentLength -= 1
 
-if writeRecordsToCsv(unsentRecords, unsentMailsCsvPath):
-    print("Unsent records written to file")
-else:
-    print("No unsent records")
+        i += 1
 
 
-print("=======================================================")
-print(f"Total records: {totalRecords}")
-print(f"Successful mails: {totalRecords - unsentLength}")
-print(f"Unsuccessful mails: {unsentLength}")
+except Exception as ex:
+    print("[!] AN ERROR OCCOURED:-")
+    print(ex)
+
+finally:
+    print("[+] Writing data to files before exiting...")
+
+    if writeRecordsToCsv(sentRecords, sentMailsCsvPath):
+        print("   [+] Sent records written to file")
+    else:
+        print("   [+] No sent records to write.")
+
+    if writeRecordsToCsv(unsentRecords, unsentMailsCsvPath):
+        print("   [+] Unsent records written to file")
+    else:
+        print("   [+] No unsent records to write.")
+
+
+print("\n\n======== OPERATION SUMMARY ========")
+print(f"\nTotal records to send: {totalRecords}")
+print(f"\nSuccessful mails: {totalRecords - unsentLength}")
+print(f"--> Saved in: {sentMailsCsvPath}")
+
+print(f"\nUnsuccessful mails: {unsentLength}")
+print(f"--> Saved in: {unsentMailsCsvPath}")
